8644e9e02b444e5774d9a2a2ebfad063f69d01bd
dinesha17 <dineshkad17@gmail.com>
2023-07-21 20:45:30 +0530

    a4

--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +9,31 @@ export class ApiService {
 
   constructor(private http:HttpClient) { }
   //create the post
-  poststudent(){
-    return this.http.post<any>("")
+  poststudent(data:any){
+    return this.http.post<any>("http://localhost:3000/posts",data)
+    .pipe(map((res:any) =>{
+       return res;
+    }))
+  }
+  //get
+  getstudent(){
+    return this.http.get<any>("http://localhost:3000/posts")
+    .pipe(map((res:any) =>{
+      return res;
+    }))
+  }
+  //update
+  updatestudent(data:any,id:number){
+    return this.http.put<any>("http://localhost:3000/posts/"+id,data)
+    .pipe(map((res:any)=>{
+      return res;
+    }))
+  }
+  //deleted
+  deletestudent(id:number){
+    return this.http.delete("http://localhost:3000/posts/"+id)
+    .pipe(map((res:any)=>{
+      return res;
+    }))
   }
 }
--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { studentdata } from './student.model';
+import { ApiService } from '../shared/api.service';
 
 @Component({
   selector: 'app-student',
@@ -10,10 +12,10 @@ export class StudentComponent implements OnInit {
 
   showadd! : boolean;
   showupdate!:boolean
-
-
+allstudentdata:any;
+   studentmodel:studentdata=new studentdata
   formvalue!:FormGroup
-  constructor(private formbuilder:FormBuilder){
+  constructor(private formbuilder:FormBuilder,private apiservices:ApiService){
 
   }
 
@@ -23,7 +25,8 @@ ngOnInit(): void {
      email:['',Validators.required],
      phonenumber:['',Validators.required],
      city:['',Validators.required]
-  })
+  });
+  this.getdata();
   
 }
 add(){
@@ -34,5 +37,28 @@ update(){
 this.showupdate=true;
 this.showadd=false;
 }   
+addstudent(){
+  debugger
+  
+  this.studentmodel.name = this.formvalue.value.name;
+  this.studentmodel.email = this.formvalue.value.email;
+  this.studentmodel.phonenumber = this.formvalue.value.phonenumber;
+  this.studentmodel.city = this.formvalue.value.city;
 
+  this.apiservices.poststudent(this.studentmodel).subscribe(res=>{
+    console.log(res)
+    this.formvalue.reset()
+    alert("record added sucessfully")
+  },
+  err=>{
+    alert("something wrongpls check me?")
+  }
+  )
+}
+//getdata
+getdata(){
+  this.apiservices.getstudent().subscribe(res=>{
+    this.allstudentdata = res;
+  })
+}
 }
