121377cdd6d2affe90fff2fc32f2ea63be12b429
dinesha17 <dineshkad17@gmail.com>
2023-07-21 18:03:10 +0530

    a3

--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-student',
   templateUrl: './student.component.html',
   styleUrls: ['./student.component.css']
 })
-export class StudentComponent {
+export class StudentComponent implements OnInit {
+
+  showadd! : boolean;
+  showupdate!:boolean
+
+
+  formvalue!:FormGroup
+  constructor(private formbuilder:FormBuilder){
+
+  }
+
+ngOnInit(): void {
+  
+}
+add(){
+ this.showadd=true;
+ this.showupdate= false;
+} 
+update(){
+this.showupdate=true;
+this.showadd=false;
+}   
 
 }
